db36d1b63a23ddb996560ce78f036461d458cc4a
kdutka2 <kdutka2@illinois.edu>
2023-03-29 17:49:49 -0500

    First commit

---
+++ trayapp.py
@@ -0,0 +1,194 @@
+import os
+import sys
+import traywindow
+from PyQt5 import QtWidgets, QtGui
+from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
+import requests
+import json
+
+class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+
+    zipCode = "60193"
+
+    def __init__(self, icon, parent=None, app=None):
+        QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
+
+        self.makeContextMenu(parent, app)
+
+        self.activated.connect(self.systemIconClick)
+
+        app.focusChanged.connect(self.on_focusChanged)
+
+        self.getData()
+
+        self.updateCurrentTemp()
+
+        self.makeIcon()
+
+    def getData(self):
+        
+        self.getLngLat()
+        self.weatherURLs()
+        self.getWeatherData()
+        self.getWeekdays()
+
+    def getLngLat(self):
+
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        cacheName = cwd + '\\' + 'cache.json'
+
+        with open(cacheName, 'r') as jsonFile:
+            self.cache = json.load(jsonFile)
+
+        if self.zipCode not in self.cache:
+            URL = "https://www.zipcodeapi.com/rest/{0}/info.json/{1}/degrees"
+            URL = URL.format(self.cache["zipApiKey"], self.zipCode)
+            
+            r = requests.get(url = URL)
+            data = r.json()
+
+            self.cache[self.zipCode] = {}
+            self.cache[self.zipCode]["lng"] = data["lng"]
+            self.cache[self.zipCode]["lat"] = data["lat"]
+
+            json_object = json.dumps(self.cache, indent=4)
+            with open(cacheName, 'w') as jsonFile:
+                jsonFile.write(json_object)
+
+    def weatherURLs(self):
+
+        lat = self.cache[self.zipCode]["lat"]
+        lng = self.cache[self.zipCode]["lng"]
+
+        URL =  "https://api.weather.gov/points/{lat},{lng}"
+        URL = URL.format(lat=lat, lng=lng)
+        
+        r = requests.get(url = URL)
+        data = r.json()
+        
+        forecastHourlyURL = data["properties"]["forecastHourly"]
+        self.weatherURL = forecastHourlyURL
+
+    def getWeatherData(self):
+
+        r = requests.get(url = self.weatherURL)
+        data = r.json()
+
+        periods = data["properties"]["periods"]
+
+        idx = int(periods[0]["startTime"][11:13])
+
+        self.x = []
+        self.temps = []
+        self.daylight = []
+        self.precipitation = []
+
+        for period in periods:
+            self.x.append(idx)
+            idx += 1
+
+            self.temps.append(period["temperature"])
+            self.daylight.append(period["isDaytime"])
+            self.precipitation.append(period["probabilityOfPrecipitation"]["value"])
+
+    def getWeekdays(self):
+        self.days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+        weekday = datetime.today().weekday() + 1
+
+        self.days = self.days[weekday:] + self.days[:weekday]
+
+    def updateCurrentTemp(self):
+        currentDateAndTime = datetime.now()
+
+        hour = currentDateAndTime.hour
+        minute = currentDateAndTime.minute
+
+        self.curTime = hour + minute / 60
+
+        idx = hour - self.x[0]
+        self.temp = (1 - minute / 60) * self.temps[idx] + (minute / 60) * self.temps[idx + 1]
+
+    def makeContextMenu(self, parent, app):
+        menu = QtWidgets.QMenu(parent)
+
+        prefs_action = menu.addAction('Window')
+        prefs_action.triggered.connect(self.openWindows)
+
+        quit_action = menu.addAction('Quit')
+        quit_action.triggered.connect(app.quit)
+
+        self.setContextMenu(menu)
+
+    def systemIconClick(self, reason):
+        # self.openWindows()
+        if reason == QtWidgets.QSystemTrayIcon.Context:
+            # print("Right Click")
+            pass
+        if reason == QtWidgets.QSystemTrayIcon.Trigger:
+            # print("Left Click")
+            self.openWindows()
+
+    def openWindows(self):
+        self.window = traywindow.MainWindow(self)
+
+        # https://stackoverflow.com/a/39064225
+        # https://stackoverflow.com/a/26411990
+        ag = QtWidgets.QDesktopWidget().availableGeometry()
+        sg = QtWidgets.QDesktopWidget().screenGeometry()
+
+        titleBarHeight = QtWidgets.QStyle.PM_TitleBarHeight
+
+        widget = self.window.size()
+
+        x = int(ag.width() - 1.5 * widget.width())
+        y = int(2 * ag.height() - sg.height() - widget.height() + 0 * titleBarHeight)
+
+        self.window.move(x, y)
+        self.window.show()
+
+        self.window.setFocus(True)
+        self.window.raise_()
+        self.window.activateWindow()
+    
+    def on_focusChanged(self):
+        if not self.window.isActiveWindow():
+            self.window.close()
+
+    def makeIcon(self):
+        # Move to trayIcon
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        filename = cwd + '\\' + 'icon.png'
+
+        curTemp = "%d" % int(round(self.temp))
+
+        fnt = ImageFont.truetype('arial.ttf', 28)
+
+        # create new image
+        image = Image.new(mode = "RGBA", size = (32,32), color=(0, 0, 0, 0))
+        draw = ImageDraw.Draw(image)
+        draw.text((0,0), curTemp, font=fnt, fill=(255,255,255, 255))
+
+        image.save(filename, 'PNG')
+
+        self.updateIcon()
+
+    def updateIcon(self): 
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        icon = cwd + '\\' + 'icon.png'
+        icon = QtGui.QIcon(icon)
+        self.setIcon(icon)
+
+
+def main(image):
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    trayIcon = SystemTrayIcon(QtGui.QIcon(image), w, app)
+    trayIcon.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    icon = cwd + '\\' + 'icon.png'
+    main(icon)
---
+++ traywindow.py
@@ -0,0 +1,221 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QCursor, QFont
+import pyqtgraph as pg
+
+class MainWindow(QMainWindow):
+
+    moveFlag  = False
+    prevTime = -1
+
+    def __init__(self, trayIcon):
+        QMainWindow.__init__(self)
+
+        self.trayIcon = trayIcon
+
+        self.setFixedSize(QSize(660, 420))    
+        self.setWindowTitle("Weather") 
+
+        self.makeLayout()
+
+        pg.setConfigOptions(antialias=True)
+
+        self.setGraph()
+        self.plotCurrentTime()
+        self.plotRegions()
+
+        self.makeButtons()
+
+        # self.setWindowFlags(QtCore.Qt.FramelessWindowHint) 
+        
+
+    def makeLayout(self):
+        # Starts basic layout
+        self.layout = QtWidgets.QVBoxLayout()
+
+        self.centralWidget = QWidget()
+        self.centralWidget.setLayout(self.layout)
+
+        self.setCentralWidget(self.centralWidget)
+
+    def makeButtons(self):
+        # Day buttons
+        self.btnRow = QtWidgets.QHBoxLayout()
+
+        self.btns = []
+
+        for i in range(7):
+            # Each Btn
+            self.btns.append(QPushButton(self.trayIcon.days[i], self))
+            self.btns[-1].setFixedSize(80, 80)
+            self.btns[-1].clicked.connect(self.chooseDayWrapper(i))
+            self.btnRow.addWidget(self.btns[-1])
+        
+        self.layout.addLayout(self.btnRow)
+
+    def chooseDayWrapper(self, i):
+        # Use for attaching chooseDay to the day buttons
+        return lambda: self.chooseDay(i)
+
+    def chooseDay(self, day):
+        # Update graph for future days
+        self.day = day
+
+        self.graphWidget.setXRange(day * 24, (day + 1) * 24, padding=0)
+
+    def plotRegions(self):
+        nightBrush = pg.mkBrush((0, 0, 0, 50))
+        dayBrush = pg.mkBrush((0, 125, 125, 50))
+
+        pen = pg.mkPen(None)
+        for time, daylight in zip(self.trayIcon.x, self.trayIcon.daylight):
+            brush = dayBrush if daylight else nightBrush
+            region = pg.LinearRegionItem(values=(time, time+1), pen=pen, brush=brush, movable=False)
+            self.graphWidget.addItem(region)
+        
+    def plotCurrentTime(self):
+
+        # Make text for current time
+        self.trayIcon.updateCurrentTemp()
+
+        color = (0, 0, 0)
+        anchor = (0.5, 1) # Where the "center point" is located for positioning
+        text = "%d" % int(round(self.trayIcon.temp, 0))
+
+        text = pg.TextItem(text, anchor=anchor, color=color)
+        text.setPos(self.trayIcon.curTime, self.trayIcon.temp + 3)
+
+        font = QFont()
+        font.setStyleHint(QFont.SansSerif)
+        font.setPointSize(12)
+        text.setFont(font)
+
+        self.graphWidget.addItem(text)
+
+        self.graphWidget.plot([self.trayIcon.curTime], [self.trayIcon.temp], pen=None, symbol='o')
+
+    def makeClickTemp(self, x):
+
+        time = max(self.trayIcon.x[0], x)
+
+        intTime = max(0, int(time) - self.trayIcon.x[0])
+        timePer = time % 1
+        temp = self.trayIcon.temps[intTime] * (1 - timePer) + self.trayIcon.temps[intTime + 1] * timePer
+
+
+        color = (50, 50, 50)
+        anchor = (0.5, 1) # Where the "center point" is located for positioning
+        text = "%d" % int(round(temp, 0))
+
+        if "mouseMarker" not in vars(self):
+            self.text = pg.TextItem(text, anchor=anchor, color=color)
+            self.text.setPos(time, temp + 3)
+
+            font = QFont()
+            font.setStyleHint(QFont.SansSerif)
+            font.setPointSize(12)
+            self.text.setFont(font)
+
+            self.graphWidget.addItem(self.text)
+            self.mouseMarker = self.graphWidget.plot([time], [temp], pen=None, symbol='o')
+            self.mouseMarkerFirstX = time
+            self.mouseMarkerFirstY = temp
+        else:
+            if self.prevTime == intTime:
+                self.text.hide()
+                self.mouseMarker.hide()
+                
+                self.prevTime = -1
+            else:
+                self.text.setPos(time, temp + 3)
+                self.text.setText("%d" % temp)
+                self.mouseMarker.setPos(time - self.mouseMarkerFirstX, temp - self.mouseMarkerFirstY)
+                
+                self.text.show()
+                self.mouseMarker.show()
+
+                self.prevTime = intTime
+
+
+    def setGraph(self):
+        # Graph area
+        self.graphWidget = pg.PlotWidget()
+        self.graphWidget.scene().sigMouseClicked.connect(self.mouse_clicked)  
+        self.graphWidget.setFixedSize(640, 320)
+
+        self.layout.addWidget(self.graphWidget)
+
+        # Light Blue Line at 32F for comparisons
+        pen = pg.mkPen(color=(128, 128, 255), width=3)
+        self.freezingLine = self.graphWidget.plot([0, 7 * 24], [32, 32], pen=pen)
+        self.freezingLine.setAlpha(0.4, False)
+
+        # Make Line and update line to data
+        pen = pg.mkPen(color=(255, 0, 0), width=3) 
+        self.line = self.graphWidget.plot([0], [1], pen=pen)
+        self.updateGraph()
+
+        # Background and scrolling
+        self.graphWidget.setBackground('w')
+        self.graphWidget.setMouseEnabled(x=True, y=False)
+
+        # Range/Domain and gridlines
+        self.graphWidget.setXRange(0, 24, padding=0)
+        self.graphWidget.setYRange(0, 100, padding=0)
+        self.graphWidget.showGrid(x=True, y=True)
+        self.graphWidget.setLimits(xMin=0, xMax=7*24)
+
+        # Day Separators
+        pen = pg.mkPen(color=(0, 0, 0), width=3)
+        for day in range(8):
+            self.graphWidget.plot([day * 24, day * 24], [0, 100], pen=pen)
+
+
+        # X-axis Labels
+        xLabels = ["{0}{1}".format((i - 1) % 12 + 1, "pm" if i >= 12 else "am") for i in range(24)]
+        xLabels = 7 * xLabels
+        xLabels = dict(enumerate(xLabels))
+        xax = self.graphWidget.getAxis('bottom')
+        xax.setTicks([list(xLabels.items())[::3]])
+        self.graphWidget.setAxisItems(axisItems = {'bottom': xax})
+
+    def updateGraph(self):
+        # Updates line to current/new data
+        x = self.trayIcon.x
+        y = self.trayIcon.temps
+        self.line.setData(x, y)
+
+    # https://clay-atlas.com/us/blog/2021/03/04/pyqt5-cn-hide-title-bar-move-interface/
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.moveFlag = True
+            self.movePosition = event.globalPos() - self.pos()
+            self.setCursor(QCursor(Qt.OpenHandCursor))
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+        if Qt.LeftButton and self.moveFlag:
+            self.move(event.globalPos() - self.movePosition)
+            event.accept()
+
+    def mouseReleaseEvent(self, event):
+        self.moveFlag = False
+        self.setCursor(Qt.ArrowCursor)
+
+    def mouse_clicked(self, mouseClickEvent):
+        # mouseClickEvent is a pyqtgraph.GraphicsScene.mouseEvents.MouseClickEvent
+
+        # Modified from https://stackoverflow.com/a/68183325
+        pos = mouseClickEvent.scenePos()
+        if self.graphWidget.sceneBoundingRect().contains(pos):
+            mousePoint = self.graphWidget.plotItem.vb.mapSceneToView(pos)
+            x = float("{0:.3f}".format(mousePoint.x()))
+            y = float("{0:.3f}".format(mousePoint.y()))
+            self.makeClickTemp(x)
+
+    def closeEvent(self, event):
+        self.hide()
+        # event.ignore() # just minimize the window to the tray
+        event.accept() # let the program close
